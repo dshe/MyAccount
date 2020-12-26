6bb93bb77350224537a5e25c2e532e5efe613592
DavidS <david@tan.com>
2020-12-26 20:24:57 +0800

    add transaction date fields

--- src/entities.rs
+++ src/entities.rs
@@ -1,3 +1,4 @@
+
 pub enum AccountType {
     Revenue,
     Expense,
@@ -8,31 +9,33 @@ pub enum AccountType {
 }
 
 pub struct Account {
-    pub id: u32,
+    pub id:           u32,
     pub account_type: AccountType,
-    pub name: String
+    pub name:         String
 }
 
 pub struct Journal {
-    pub id: u32,
+    pub id:   u32,
     pub name: String
 }
 
 pub struct Transaction {
-    pub id: u32,
-    pub journal: u32,
-    pub account: u32,
-    pub amount: rust_decimal::Decimal, // credit/debit: positive/negative
+    pub id:          u32,
+    pub date:        chrono::DateTime<chrono::Utc>,
+    pub journal:     u32,
+    pub account:     u32,
+    pub amount:      rust_decimal::Decimal, // credit/debit = positive/negative
     pub description: String
 }
 
 impl Transaction {
     pub fn to_string(self) -> String {
-        format!("id={}, journal={}, account={}, amount={:+} description='{}'",
-            self.id, 
-            self.journal, 
-            self.account, 
-            self.amount, 
+        format!("id={}, date={}, journal={}, account={}, amount={:+} description='{}'",
+            self.id,
+            self.date,
+            self.journal,
+            self.account,
+            self.amount,
             self.description)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,23 @@
 mod entities;
+use chrono::{Utc, TimeZone};
 
 fn main() {
 
     // create a list of accounts
     let mut accounts: Vec<entities::Account> = Vec::new();
 
-    // create a sales account and addd it to the list
+    // create a sales account and add it to the list of accounts
     let account1 = entities::Account {
-        id: 1,
-        name: "sales".to_string(),
+        id:           1,
+        name:         "sales".to_string(),
         account_type: entities::AccountType::Revenue
     };
     accounts.push(account1);
 
-    // create a cash account and addd it to the list
+    // create a cash account and add it to the list of accounts
     let account2 = entities::Account {
-        id: 2,
-        name: "cash".to_string(),
+        id:           2,
+        name:         "cash".to_string(),
         account_type: entities::AccountType::Asset
     };
     accounts.push(account2);
@@ -24,9 +25,9 @@ fn main() {
     // create a list of journals
     let mut journals: Vec<entities::Journal> = Vec::new();
 
-    // create a journal and add it to the list
+    // create a journal and add it to the list of journals
     let journal = entities::Journal {
-        id: 3,
+        id:   3,
         name: "sale".to_string()
     };
     journals.push(journal);
@@ -34,23 +35,25 @@ fn main() {
     // create a list of transactions
     let mut transactions: Vec<entities::Transaction> = Vec::new();
 
-    // add a transaction to the list for the journal
+    // add a transaction to the list of transactions
     let t = entities::Transaction {
-        id: 4,
+        id:          4,
+        date:        chrono::Utc.ymd(2020, 1, 1).and_hms_milli(0, 0, 1, 444),
         description: "processing sale".to_string(),
-        journal: 3,
-        account: 1,
-        amount: rust_decimal::Decimal::new(200, 1)
+        journal:     3,
+        account:     1,
+        amount:      rust_decimal::Decimal::new(200, 1)
     };
     transactions.push(t);
 
-    // add another transaction to the list for the journal
+    // add another transaction to the list of transactions
     let t = entities::Transaction {
-        id: 5,
+        id:          5,
+        date:        Utc.ymd(2020, 1, 1).and_hms_milli(0, 0, 1, 444),
         description: "collecting cash".to_string(),
-        journal: 3,
-        account: 2,
-        amount: rust_decimal::Decimal::new(-200, 1)
+        journal:     3,
+        account:     2,
+        amount:      rust_decimal::Decimal::new(-200, 1)
     };
     transactions.push(t);
 
