33a7686ee28ea7998a8d777a8974755449aa9b57
DavidS <david@tan.com>
2020-12-27 21:29:10 +0800

    use references

--- src/entities.rs
+++ src/entities.rs
@@ -1,40 +1,38 @@
-
 pub enum AccountType {
     Revenue,
     Expense,
     Asset,
     Liability,
-    Capital,
-    IncomeTax
+    Equity
 }
 
 pub struct Account {
-    pub account_id:   u32,
+    pub account_id:   u64,
     pub account_type: AccountType,
     pub name:         String
 }
 
 pub struct Journal {
-    pub journal_id: u32,
+    pub journal_id: u64,
     pub title:      String
 }
 
 pub struct Transaction {
-    pub transaction_id: u32,
+    pub transaction_id: u64,
+    pub journal_id:     u64,
+    pub account_id:     u64,
     pub date:           chrono::DateTime<chrono::Utc>,
-    pub journal_id:     u32,
-    pub account_id:     u32,
     pub amount:         rust_decimal::Decimal, // credit/debit = positive/negative
     pub description:    String
 }
 
 impl Transaction {
-    pub fn to_string(self) -> String {
-        format!("transaction_id={}, date={}, journal_id={}, account_id={}, amount={:+} description='{}'",
+    pub fn to_string(&self) -> String {
+        format!("transaction_id={}, journal_id={}, account_id={}, date={}, amount={:+} description='{}'",
             self.transaction_id,
-            self.date,
-            self.journal_id,
+             self.journal_id,
             self.account_id,
+            self.date,
             self.amount,
             self.description)
     }
--- src/main.rs
+++ src/main.rs
@@ -4,61 +4,59 @@ use chrono::TimeZone;
 fn main() {
 
     // create a list of accounts
-    let mut accounts: Vec<entities::Account> = Vec::new();
+    let mut accounts: Vec<&entities::Account> = Vec::new();
 
     // create a sales account and add it to the list of accounts
     let account1 = entities::Account {
         account_id:   1,
-        name:         "sales".to_string(),
+        name:         "sales account".to_string(),
         account_type: entities::AccountType::Revenue
     };
-    accounts.push(account1);
+    accounts.push(&account1);
 
     // create a cash account and add it to the list of accounts
     let account2 = entities::Account {
         account_id:   2,
-        name:         "cash".to_string(),
+        name:         "accounts receivable".to_string(),
         account_type: entities::AccountType::Asset
     };
-    accounts.push(account2);
+    accounts.push(&account2);
 
     // create a list of journals
-    let mut journals: Vec<entities::Journal> = Vec::new();
+    let mut journals: Vec<&entities::Journal> = Vec::new();
 
     // create a journal and add it to the list of journals
     let journal = entities::Journal {
         journal_id: 1,
-        title:      "sale".to_string()
+        title:      "sale journal".to_string()
     };
-    journals.push(journal);
+    journals.push(&journal);
 
     // create a list of transactions
-    let mut transactions: Vec<entities::Transaction> = Vec::new();
+    let mut transactions: Vec<&entities::Transaction> = Vec::new();
 
     // add a transaction to the list of transactions
     let transaction1 = entities::Transaction {
         transaction_id: 1,
         date:           chrono::Utc.ymd(2020, 1, 1).and_hms_milli(0, 0, 1, 444),
-        description:    "processing sale".to_string(),
+        description:    "sale of some item".to_string(),
         journal_id:     1,
         account_id:     1,
         amount:         rust_decimal::Decimal::new(200, 1)
     };
-    transactions.push(transaction1);
+    transactions.push(&transaction1);
 
     // add another transaction to the list of transactions
     let transaction2 = entities::Transaction {
         transaction_id: 2,
         date:           chrono::Utc.ymd(2020, 1, 1).and_hms_milli(0, 0, 1, 444),
-        description:    "collecting cash".to_string(),
+        description:    "future payment expected".to_string(),
         journal_id:     1,
         account_id:     2,
         amount:         rust_decimal::Decimal::new(-200, 1)
     };
-    transactions.push(transaction2);
+    transactions.push(&transaction2);
 
     // print all transactions
-    for tt in transactions {
-        println!("Transaction: {0}", tt.to_string());
-    }
+    transactions.iter().for_each(|t| println!("Transaction: {0}", t.to_string()));
 }
